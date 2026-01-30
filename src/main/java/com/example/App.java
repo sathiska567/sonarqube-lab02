109b74dcacd6f6af32b14ba7e133574282dd82f9
sathiska-intrinsic-tech <sasindusathiska@gmail.com>
2026-01-30 12:23:27 +0530

    issue 04 fixed

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,7 +9,9 @@ public class App {
 
     public static void main(String[] args) throws Exception {
         Calculator calc = new Calculator();
-        logger.info(String.valueOf(calc.calculate(10, 5, "add-again")));
+        if (calc != null) {
+            logger.info(String.valueOf(calc.calculate(10, 5, "add-again")));
+        }
 
         UserService service = new UserService();
         service.findUser("admin");
