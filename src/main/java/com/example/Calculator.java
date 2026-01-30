f5d1c64fa06a914bd7accd328a2c172c403fba57
sathiska-intrinsic-tech <sasindusathiska@gmail.com>
2026-01-30 12:34:08 +0530

    issue 04 fixed

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -2,36 +2,38 @@ package main.java.com.example;
 
 public class Calculator {
 
-    // Refactored: no duplicated logic
+    // EVEN WORSE: longer, more complex, duplicated logic
     public int calculate(int a, int b, String op) {
-        switch (op) {
-            case "add":
-            case "add-again":
-                return a + b;
-            case "sub":
-            case "sub-again":
-                return a - b;
-            case "mul":
-                return a * b;
-            case "div":
-                return b == 0 ? 0 : a / b;
-            case "mod":
-                return a % b;
-            case "pow":
-                int result = 1;
-                for (int i = 0; i < b; i++) {
-                    result *= a;
-                }
-                return result;
-            default:
+        if (op.equals("add")) {
+            return a + b;
+        } else if (op.equals("add-again")) {
+            return a + b; // DUPLICATION
+        } else if (op.equals("sub")) {
+            return a - b;
+        } else if (op.equals("sub-again")) {
+            return a - b; // DUPLICATION
+        } else if (op.equals("mul")) {
+            return a * b;
+        } else if (op.equals("div")) {
+            if (b == 0) {
                 return 0;
+            } else {
+                return a / b;
+            }
+        } else if (op.equals("mod")) {
+            return a % b;
+        } else if (op.equals("pow")) {
+            int result = 1;
+            for (int i = 0; i < b; i++) {
+                result = result * a;
+            }
+            return result;
+        } else {
+            return 0;
         }
     }
 
-    // Optional helper methods (for coverage / tests)
-    public int addAgain(int a, int b) {
-        return a + b;
-    }
+    public int addAgain(int a, int b) {return a + b;}
 
     public int sumValues(int a, int b) {
         return a + b;
