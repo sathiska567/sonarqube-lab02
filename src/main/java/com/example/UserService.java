288dcaf8238c61b53df26512ed24ffa3fe23c973
sathiska-intrinsic-tech <sasindusathiska@gmail.com>
2026-01-30 12:21:27 +0530

    issue 03 fixed

--- src/main/java/com/example/UserService.java
+++ src/main/java/com/example/UserService.java
@@ -12,17 +12,11 @@ public class UserService {
 
     // VULNERABILITY: SQL Injection
     public void findUser(String username) throws SQLException {
-
-        Connection conn
-                = DriverManager.getConnection("jdbc:mysql://localhost/db",
-                        "root", password);
-
-        Statement st = conn.createStatement();
-
-        String query
-                = "SELECT * FROM users WHERE name = '" + username + "'";
-
-        st.executeQuery(query);
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
+             Statement st = conn.createStatement()) {
+            String query = "SELECT * FROM users WHERE name = '" + username + "'";
+            st.executeQuery(query);
+        }
     }
 
     // SMELL: Unused method
@@ -32,10 +26,11 @@ public class UserService {
 
     // EVEN WORSE: another SQL injection
     public void deleteUser(String username) throws SQLException {
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
-        Statement st = conn.createStatement();
-        String query = "DELETE FROM users WHERE name = '" + username + "'";
-        st.execute(query);
+        try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "root", password);
+             Statement st = conn.createStatement()) {
+            String query = "DELETE FROM users WHERE name = '" + username + "'";
+            st.execute(query);
+        }
     }
 
     public class UserServiceException extends Exception {
